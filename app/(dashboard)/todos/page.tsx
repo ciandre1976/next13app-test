e16153304726d0dbdc8a08323b925e5bba43fc0d
AleksandarStanisic01 <a.stanisic.codealkonost@gmail.com>
2023-10-09 12:01:13 +0200

    routing test

---
+++ app/(dashboard)/todos/page.tsx
@@ -0,0 +1,5 @@
+const TodoPage = () => {
+  return <div>todos</div>;
+};
+
+export default TodoPage;
---
+++ app/docs/[...id]/page.tsx
@@ -0,0 +1,3 @@
+export default function IDpage({ params }) {
+  return <div>Id : {params.id}</div>;
+}
